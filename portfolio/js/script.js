b4468cdbb65c78726a09bbd4016d079c1e5c505b
吉澤映彦 <yoshizawateruhiko@yoshizawaeihikonoMacBook-Air.local>
2019-02-10 03:34:43 +0900

    Create portfolio

---
+++ portfolio/js/script.js
@@ -0,0 +1,22 @@
+$(function(){
+
+  $('.menu').click(function(){
+      $('.modal').fadeIn();
+      $('.menu').fadeOut();
+  });
+
+  $('#menu2').click(function(){
+      $('.modal').fadeOut();
+      $('.menu').fadeIn();
+  });
+
+  $(document).ready(function(){
+      $('.sentence2').slideDown(2000);
+  });
+
+	$(window).on('scroll touchmove', function(){
+			$('.menu').stop();
+			$('.menu').css('display', 'none').fadeIn('50');
+	});
+
+});
